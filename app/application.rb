b5833fb0acaf6dd4711e7483a783b05dd6f0e089
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-23 01:02:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     num_1 = Kernel.rand(1..20)
-num_2 = Kernel.rand(1..20)
-num_3 = Kernel.rand(1..20)
-    resp.finish
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+    
   end
  
 end
